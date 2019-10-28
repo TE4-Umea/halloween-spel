f932f5a2969f5c66405420b3e81d5f4cb679b4fa
Olle Kaiser <olle.kaiser@learnet.se>
2019-10-28 10:16:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Hur ni lägger till era spel här!
+
+Clona det här repot
+    
+    git clone https://github.com/te4umea2019/halloween-spel
+
+Skapa ett cover art till erat spel med samma upplösning och dimensioner som /img/template/cover.png, (tip: dra bara in template covern i photoshop och arbeta direkt på den.)
+
+Ta också några screenshots från erat spel och lägg dem, samt ditt cover i en ny mapp i /img som du döper till ditt namn eller namnet av ditt spel (inga stora bokstäver, mellanslag eller bindesträck) (t.ex /img/ollkai/).
+
+Kopiera /js/template.js i samma map och döp den till samma sak som du döpte din bildmapp (t.ex /js/ollkai.js).
+
+Ändra allt i den så det stämmer med erat spel.
+
+Skapa en ny branch med samma namn
+
+    git checkout -b ollkai
+
+Adda, Committa, Pusha och skapa en ny pull request på Github. 
+
+👍
---
+++ js/template.js
@@ -0,0 +1,29 @@
+games.push({
+
+    name: 'Namnet på ditt spel',
+    /* Ditt namn */
+    creator: 'Förnamn Efternamn',
+
+    /* Om du har en readme i din github länk behöver du inte en description. Iståfall lämna den tom. */
+    description: '',
+    /* Välj en eller flera katergorier till ditt spel, dela upp dem mellanslag 
+       om du vill lägga till kategerioer, skriv dem också här under i template filen! */
+    tags: 'Platformer Puzzle Tower-defense Card Multiplayer Online Co-op',
+
+    /* Språk och miljö-tagger, dela upp med mellanslag */
+    /* Samma gäller ang tags. */
+    language_tags: 'TS HTML5 nodejs CSS Vue Python PHP',
+    
+    /* Länk till din github. Din Github kommer länkas på spelsidan 
+       men din readme för spelet kommer också visas som en beskrivning */
+    github: 'https://github.com/YourUsername/YourGame',
+
+    cover_art: 'img/template/cover.png',
+
+    /* Du kan ha hur många screenshots som du vill */
+    screenshots: [
+        'img/template/sh0.png',
+        'img/template/sh1.png',
+        'img/template/sh2.png'
+    ]
+})
