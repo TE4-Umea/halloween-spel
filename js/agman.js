58299178c818835f657d654ffd2a45b18f0a143f
august.moller <ag.man@live.se>
2019-10-29 13:55:50 +0100

    Agman's game

---
+++ js/agman.js
@@ -0,0 +1,32 @@
+var games = games || []
+games.push({
+
+    name: 'Pumpkin Snake',
+
+    /* Ditt namn */
+    creator: 'August Möller',
+
+    /* Länk till erat spel! */
+    link: 'http://snake.agman.xyz/',
+
+    /* Om du har en readme i din github länk behöver du inte en description. Iståfall lämna den tom. */
+    description: 'Snake but you play as a pumpkin and is scary',
+    /* Välj en eller flera katergorier till ditt spel, dela upp dem mellanslag 
+       om du vill lägga till kategerioer, skriv dem också här under i template filen! */
+    tags: 'Snake',
+
+    /* Språk och miljö-tagger, dela upp med mellanslag */
+    /* Samma gäller ang tags. */
+    language_tags: 'javascript',
+    
+    /* Länk till din github. Din Github kommer länkas på spelsidan 
+       men din readme för spelet kommer också visas som en beskrivning */
+    github: 'https://github.com/Agman10/Pumpkin-Snake',
+
+    cover_art: 'img/agman/cover.png',
+
+    /* Du kan ha hur många screenshots som du vill */
+    screenshots: [
+        'img/agman/sh0.png'
+    ]
+})
