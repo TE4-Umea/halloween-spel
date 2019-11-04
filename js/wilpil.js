234678003f90804c116779dcb24e676ca4f5be76
digitalmoleman <digitalmoleguy@gmail.com>
2019-11-04 15:11:31 +0100

    added roller

---
+++ js/wilpil.js
@@ -0,0 +1,34 @@
+var games = games || []
+games.push({
+
+    name: 'Roller',
+
+    /* Ditt namn */
+    creator: 'Wille Pilquist',
+
+    /* Länk till erat spel! */
+    link: 'https://digitalmoleman.github.io/roller/',
+
+    /* Om du har en readme i din github länk behöver du inte en description. Iståfall lämna den tom. */
+    description: '',
+    /* Välj en eller flera katergorier till ditt spel, dela upp dem mellanslag 
+       om du vill lägga till kategerioer, skriv dem också här under i template filen! */
+    tags: 'Platformer Metroidvania',
+
+    /* Språk och miljö-tagger, dela upp med mellanslag */
+    /* Samma gäller ang tags. */
+    language_tags: 'JavaScript',
+    
+    /* Länk till din github. Din Github kommer länkas på spelsidan 
+       men din readme för spelet kommer också visas som en beskrivning */
+    github: 'https://github.com/DigitalMoleMan/roller',
+
+    cover_art: 'img/template/roller.png',
+
+    /* Du kan ha hur många screenshots som du vill */
+    screenshots: [
+        'img/template/sh0.png',
+        'img/template/sh1.png',
+        'img/template/sh2.png'
+    ]
+})
